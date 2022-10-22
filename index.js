3dc1150b2304e4b70843d1b85bac1f48f9743756
trteel1 <tateelamino@gmail.com>
2022-10-23 02:14:46 +0300

    passing

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(free){
+  
+  if (free <= 400) {
+   return "This one is on me!";
+    }
+ else if (free >400 && free<2000){
+  return "That will be twenty bucks.";
+ }
+ else if (free >2000 && free<=2500 ){
+   return "I will gladly take your thirty bucks.";
+ }
+  else if  (free>2500 ){
+   return "No can do.";
+  }
+}
+console.log(scuberGreetingForFeet(199));
+
+function ternaryCheckCity(city){
+  
+  return (city=== "NYC" ? 'Ok, sounds good.' : 'No go.');
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  
+  if (tip==='generous'){
+    return 'Thank you so much.';
+  }
+   else if  (tip==='not as generous'){
+    return 'Thank you.';
+  }
+   else { return 'Bye.';
+   }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
